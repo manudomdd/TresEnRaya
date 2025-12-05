4952fffe131c8d6c3170614a2f8406623b707ea2
Manu <manu@pop-os.home>
2025-12-05 23:28:59 +0100

    Pantalla inicio

---
+++ src/gui/FormInicio.java
@@ -0,0 +1,292 @@
+package gui;
+
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+public class FormInicio extends javax.swing.JFrame {
+    
+    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(FormInicio.class.getName());
+
+    public FormInicio() {
+        init();       
+    }
+    
+    public void init() {
+        this.setUndecorated(true);
+        initComponents(); 
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        panelFondo.requestFocus();
+        
+        Border border = BorderFactory.createLineBorder(new Color(243, 211, 246), 2); 
+        panelBoton.setBorder(border);
+    }
+    
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelFondo = new javax.swing.JPanel();
+        lblCierre = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        txtJugador1 = new javax.swing.JTextField();
+        txtJugador2 = new javax.swing.JTextField();
+        panelBoton = new javax.swing.JPanel();
+        lblIniciar = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        panelFondo.setBackground(new java.awt.Color(14, 19, 49));
+        panelFondo.setPreferredSize(new java.awt.Dimension(416, 315));
+        panelFondo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                panelFondoMousePressed(evt);
+            }
+        });
+        panelFondo.setLayout(null);
+
+        lblCierre.setFont(new java.awt.Font("Fira Mono", 0, 24)); // NOI18N
+        lblCierre.setForeground(new java.awt.Color(240, 192, 255));
+        lblCierre.setText("X");
+        lblCierre.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblCierre.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblCierreMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblCierreMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblCierreMouseExited(evt);
+            }
+        });
+        panelFondo.add(lblCierre);
+        lblCierre.setBounds(390, 10, 20, 20);
+
+        jLabel1.setFont(new java.awt.Font("Fira Mono", 0, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("TRES EN RAYA");
+        panelFondo.add(jLabel1);
+        jLabel1.setBounds(0, 10, 420, 44);
+
+        txtJugador1.setBackground(new java.awt.Color(214, 252, 249));
+        txtJugador1.setFont(new java.awt.Font("Fira Mono", 0, 24)); // NOI18N
+        txtJugador1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtJugador1.setText("Jugador 1");
+        txtJugador1.setBorder(null);
+        txtJugador1.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtJugador1FocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txtJugador1FocusLost(evt);
+            }
+        });
+        txtJugador1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                txtJugador1MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                txtJugador1MouseExited(evt);
+            }
+        });
+        txtJugador1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtJugador1KeyTyped(evt);
+            }
+        });
+        panelFondo.add(txtJugador1);
+        txtJugador1.setBounds(100, 90, 230, 40);
+
+        txtJugador2.setBackground(new java.awt.Color(214, 252, 249));
+        txtJugador2.setFont(new java.awt.Font("Fira Mono", 0, 24)); // NOI18N
+        txtJugador2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        txtJugador2.setText("Jugador 2");
+        txtJugador2.setBorder(null);
+        txtJugador2.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtJugador2FocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txtJugador2FocusLost(evt);
+            }
+        });
+        txtJugador2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                txtJugador2MouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                txtJugador2MouseExited(evt);
+            }
+        });
+        txtJugador2.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtJugador2KeyTyped(evt);
+            }
+        });
+        panelFondo.add(txtJugador2);
+        txtJugador2.setBounds(100, 160, 230, 40);
+
+        panelBoton.setBackground(new java.awt.Color(42, 22, 79));
+
+        lblIniciar.setFont(new java.awt.Font("Fira Mono", 1, 18)); // NOI18N
+        lblIniciar.setForeground(new java.awt.Color(241, 227, 252));
+        lblIniciar.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblIniciar.setText("INICIAR");
+        lblIniciar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblIniciar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lblIniciarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lblIniciarMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panelBotonLayout = new javax.swing.GroupLayout(panelBoton);
+        panelBoton.setLayout(panelBotonLayout);
+        panelBotonLayout.setHorizontalGroup(
+            panelBotonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblIniciar, javax.swing.GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE)
+        );
+        panelBotonLayout.setVerticalGroup(
+            panelBotonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lblIniciar, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
+        );
+
+        panelFondo.add(panelBoton);
+        panelBoton.setBounds(130, 230, 160, 50);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelFondo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelFondo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void lblCierreMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_lblCierreMouseClicked
+
+    private void lblCierreMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseEntered
+        lblCierre.setForeground(Color.red);
+    }//GEN-LAST:event_lblCierreMouseEntered
+
+    private void lblCierreMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseExited
+        lblCierre.setForeground(new Color(240, 192, 255));
+    }//GEN-LAST:event_lblCierreMouseExited
+
+    private void txtJugador1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtJugador1MouseEntered
+        Border borde = BorderFactory.createLineBorder(new Color(52, 136, 235), 2);
+        txtJugador1.setBorder(borde);
+    }//GEN-LAST:event_txtJugador1MouseEntered
+
+    private void txtJugador1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtJugador1MouseExited
+        txtJugador1.setBorder(null);
+    }//GEN-LAST:event_txtJugador1MouseExited
+
+    private void txtJugador2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtJugador2MouseEntered
+        Border borde = BorderFactory.createLineBorder(new Color(52, 136, 235), 2);
+        txtJugador2.setBorder(borde);
+    }//GEN-LAST:event_txtJugador2MouseEntered
+
+    private void txtJugador2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtJugador2MouseExited
+        txtJugador2.setBorder(null);
+    }//GEN-LAST:event_txtJugador2MouseExited
+
+    private void txtJugador1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtJugador1FocusGained
+        if (txtJugador1.getText().equals("Jugador 1")) {
+            txtJugador1.setText("");
+        }
+    }//GEN-LAST:event_txtJugador1FocusGained
+
+    private void txtJugador2FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtJugador2FocusGained
+        if (txtJugador2.getText().equals("Jugador 2")) {
+            txtJugador2.setText("");
+        }
+    }//GEN-LAST:event_txtJugador2FocusGained
+
+    private void txtJugador1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtJugador1FocusLost
+        if (txtJugador1.getText().equals("")) {
+            txtJugador1.setText("Jugador 1");
+        }
+    }//GEN-LAST:event_txtJugador1FocusLost
+
+    private void txtJugador2FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtJugador2FocusLost
+        if (txtJugador2.getText().equals("")) {
+            txtJugador2.setText("Jugador 2");
+        }
+    }//GEN-LAST:event_txtJugador2FocusLost
+
+    private void panelFondoMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_panelFondoMousePressed
+        panelFondo.requestFocus();
+    }//GEN-LAST:event_panelFondoMousePressed
+
+    private void txtJugador1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtJugador1KeyTyped
+        char character = Character.toUpperCase(evt.getKeyChar()); 
+        
+        evt.setKeyChar(character);
+        
+        if (txtJugador1.getText().length() >= 8) {
+            evt.consume(); 
+        }       
+    }//GEN-LAST:event_txtJugador1KeyTyped
+
+    private void txtJugador2KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtJugador2KeyTyped
+        char character = Character.toUpperCase(evt.getKeyChar()); 
+        
+        evt.setKeyChar(character);
+        
+        if (txtJugador2.getText().length() >= 8) {
+            evt.consume(); 
+        } 
+    }//GEN-LAST:event_txtJugador2KeyTyped
+
+    private void lblIniciarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblIniciarMouseEntered
+        panelBoton.setBackground(new Color(126, 49, 196));
+    }//GEN-LAST:event_lblIniciarMouseEntered
+
+    private void lblIniciarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblIniciarMouseExited
+        panelBoton.setBackground(new Color(42, 22, 79));
+
+    }//GEN-LAST:event_lblIniciarMouseExited
+      
+    public static void main(String args[]) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(() -> new FormInicio().setVisible(true));
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lblCierre;
+    private javax.swing.JLabel lblIniciar;
+    private javax.swing.JPanel panelBoton;
+    private javax.swing.JPanel panelFondo;
+    private javax.swing.JTextField txtJugador1;
+    private javax.swing.JTextField txtJugador2;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/principal/App.java
@@ -0,0 +1,12 @@
+package principal;
+
+import gui.FormInicio;
+
+public class App {
+
+    public static void main(String[] args) {
+       
+        FormInicio formInicio = new FormInicio(); 
+        formInicio.setVisible(true);
+    }  
+}
