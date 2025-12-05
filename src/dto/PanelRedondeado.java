cdd356142426f41ba684502df03a8501ba517883
Manu <manu@pop-os.home>
2025-12-05 23:58:40 +0100

    Changes

--- src/dto/PanelRedondeado.java
+++ src/dto/PanelRedondeado.java
@@ -1,31 +1,52 @@
-package dto;
+package dto; 
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.Shape;
 import java.awt.geom.RoundRectangle2D;
 import javax.swing.JPanel;
 
 public class PanelRedondeado extends JPanel {
-    private double arcoAncho = 50; 
-    private double arcoAltura = 50; 
-    private Color colorBorde = new Color(165, 40, 174); 
+
+    private int arcoAncho = 50;
+    private int arcoAltura = 50;
+    private Color colorBorde = new Color(165, 40, 174);
+
+    public PanelRedondeado() {
+        super();
+        setOpaque(false); 
+        setBorder(null); 
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);       
-        Graphics2D g2= (Graphics2D) g.create(); 
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        RoundRectangle2D.Double formaRedondeada = new RoundRectangle2D.Double(
-                0, 0, this.getWidth()-1, this.getHeight()-1, arcoAncho, arcoAltura
-        ); 
+        Graphics2D g2 = (Graphics2D) g.create();
         
-        g2.setColor(this.getBackground());
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        Shape formaRedondeada = new RoundRectangle2D.Double(
+                0, 0, getWidth() - 1, getHeight() - 1, arcoAncho, arcoAltura
+        );
+
+        g2.setColor(getBackground());
         g2.fill(formaRedondeada);
+
         g2.setColor(colorBorde);
         g2.draw(formaRedondeada);
-        g2.dispose(); 
-        
-    }   
+
+        g2.dispose();
+    }
+
+    public void setArc(int ancho, int altura) {
+        this.arcoAncho = ancho;
+        this.arcoAltura = altura;
+        repaint(); // Re-draw the component with new shape
+    }
+
+    public void setColorBorde(Color color) {
+        this.colorBorde = color;
+        repaint();
+    }
 }
